03b262b5e077882155407f7feb327aee69fbcb5b
Ubuntu <ubuntu@fv4v0arfkv8nheb6pj7g.auto.internal>
2026-06-23 08:40:37 +0000

    add dotenv file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,19 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.133.72', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['158.160.133.72', '127.0.0.1', 'localhost', 'labudabdab.hopto.org']
+
+CSRF_TRUSTED_ORIGINS = ['https://labudabdab.hopto.org']
 
 
 # Application definition
